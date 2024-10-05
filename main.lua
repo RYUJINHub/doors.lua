9745822e5d9dba74c850d1de6ac55579f560c957
RYUJINHub <142650366+RYUJINHub@users.noreply.github.com>
2024-10-05 18:09:16 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -66,7 +66,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Doors -",
+    Title = "Doors - ",
     SubTitle = "Kurumi Hub",
     TabWidth = 160,
     Size = UDim2.fromOffset(510, 390),
